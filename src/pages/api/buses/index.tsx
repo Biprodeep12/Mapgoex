373185187a5a92b74d3e024b167a18e432e538d9
Biprodeep Bose <bipbose123@gmail.com>
2025-09-03 03:04:15 +0530

    fix

--- src/pages/api/buses/index.tsx
+++ src/pages/api/buses/index.tsx
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import data from "@/data/pod.json";
+import { BusData } from "@/types/bus";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { busId } = req.query;
-  const bus = (data as Record<string, any>)[busId as string];
+  const bus = (data as unknown as Record<string,BusData>)[busId as string];
 
   if (!bus) {
     return res.status(404).json({ error: "Bus not found" });
