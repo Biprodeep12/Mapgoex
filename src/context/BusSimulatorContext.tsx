31c1cdf2b31dece99a894c66b70464239e97b7d1
Biprodeep Bose <bipbose123@gmail.com>
2025-10-28 03:46:03 +0530

    fix link

--- src/context/BusSimulatorContext.tsx
+++ src/context/BusSimulatorContext.tsx
@@ -69,7 +69,7 @@ export const BusSimulatorProvider = ({ children }: { children: ReactNode }) => {
   };
 
   useEffect(() => {
-    const socket = io("https://maggeoxserver.onrender");
+    const socket = io("https://maggeoxserver.onrender.com");
     socketRef.current = socket;
 
     socket.on("connect", () => {
